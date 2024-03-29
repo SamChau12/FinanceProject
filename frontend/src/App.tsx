a53f2e55f7370884ae79f0514429eea24b0f440e
Samjhana Chaudhary <samchau78@gmail.com>
2024-03-29 11:48:27 +0545

    implemented dashboard for showing company detail and its data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>FinShark Design Page</h1>
+      <h2>
+        this is FinShark's design page. This is where we will house various
+        designs aspect of App
+      </h2>
+      {/* <Table /> */}
+    </>
+  );
+};
+
+export default DesignPage;
