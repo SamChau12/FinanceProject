a37664175a2a1a82d01852c3c0904c0e381106ba
Samjhana Chaudhary <samchau78@gmail.com>
2024-04-16 10:16:02 +0545

    auth service is added for react typescript context for login and Jwt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { Toast, ToastContainer } from "react-toastify/dist/components";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
